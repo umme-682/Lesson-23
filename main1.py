50f35f3cd68387ad3c1f822adb8285dba95cd4b7
umme-682 <umme.salama144@gmail.com>
2024-12-23 14:00:45 +0500

    num

---
+++ main1.py
@@ -0,0 +1,22 @@
+def SieveOfEratosthenes(num):
+    prime = [True for i in range(num+1)]
+    
+    p = 2
+    while(p * p <= num):
+        
+        if(prime[p] == True):
+            
+            for i in range(p * p, num+1, p):
+                prime[i] = False 
+        p += 1
+        
+    for p in range(2, num+1):
+        if prime[p]:
+            print(p)
+            
+num = int(input("Enter the number: "))
+print("The followng are the prime numbers smaller")
+print("Than or equal to", num)
+SieveOfEratosthenes(num)
+
+    
